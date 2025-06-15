5f847ab22a47ab5b2166e3c64885e759f1871580
AkshayK025 <askanchan314@gmail.com>
2025-06-15 15:45:42 +0530

    Problems added in files

--- Match_Combinations/problem1.sql
+++ Match_Combinations/problem1.sql
@@ -1,3 +1,4 @@
+ SHOW Databases;
  CREATE Database Match_Combinations;
  USE Match_Combinations;
 
@@ -37,6 +38,17 @@ FROM
     teams t1 
 JOIN teams t2 on t1.team_name < t2.team_name
 
+
+--method 2 using self join
+SELECT 
+    t1.team_name,
+    t2.team_name
+FROM   
+    teams t1,teams t2
+WHERE t1.team_name < t2.team_name;
+
+
+
 -- cartensian product self join
 SELECT 
     concat(
@@ -46,3 +58,35 @@ SELECT
 FROM
     teams t1 , teams t2
 
+
+--permutation (4,3)
+SELECT 
+
+    t1.team_name AS team1,
+    t2.team_name AS team2,
+    t3.team_name AS team3
+    
+FROM 
+    teams t1
+JOIN 
+    teams t2 ON t1.team_name != t2.team_name
+JOIN 
+    teams t3 ON t3.team_name != t1.team_name AND t3.team_name != t2.team_name
+ORDER BY 
+    team1, team2, team3;
+
+
+
+--combination (4,3)
+SELECT 
+    t1.team_name AS team1,
+    t2.team_name AS team2,
+    t3.team_name AS team3
+FROM 
+    teams t1
+JOIN 
+    teams t2 ON t1.team_name < t2.team_name
+JOIN 
+    teams t3 ON t2.team_name < t3.team_name
+ORDER BY 
+    team1, team2, team3;
