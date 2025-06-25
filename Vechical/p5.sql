92db844827f76b4e210babc0f28141abb1cf3a20
AkshayK025 <askanchan314@gmail.com>
2025-06-25 18:02:50 +0530

    problem 5 solved

--- Vechical/p5.sql
+++ Vechical/p5.sql
@@ -3,4 +3,26 @@
 duplicated."
 */
 
+WITH cte as
+    (SELECT
+        full_name, 
+        email,
+        phone,
+        ROW_NUMBER() OVER(PARTITION BY email ORDER BY email) as rn_mail,
+        ROW_NUMBER() OVER(PARTITION BY phone ORDER BY phone) as rn_phone
+    FROM 
+        customers
+    )
+SELECT 
+    full_name,
+    email,
+    phone,
+    CASE
+    WHEN rn_mail > 1 OR rn_phone > 1 THEN 'Duplicate'
+    ELSE 'Not Duplicate'
+END
 
+FROM
+    cte
+ORDER BY
+    full_name;
