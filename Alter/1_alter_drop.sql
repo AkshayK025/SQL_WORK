0b38ad65c845e8a46e226a35e66ba4790fa07178
AkshayK025 <askanchan314@gmail.com>
2026-01-05 09:49:37 +0530

    alter db added

---
+++ Alter/1_alter_drop.sql
@@ -0,0 +1,20 @@
+use db_alter;
+
+-- drop first_name column from the students table use db_alter
+ALTER TABLE students
+drop column first_name;
+
+-- drop foreign key from the table enrollments use db_alter
+SHOW CREATE TABLE enrollments;      -- to view foreign key name 
+ALTER TABLE enrollments
+drop foreign key fk_student_id;
+
+-- Drop index from the table enrollments use db_alter
+SHOW INDEX FROM enrollments;        -- to view index name 
+ALTER TABLE enrollments
+DROP INDEX fk_student_id;
+
+-- Drop primary key from the table students use db_alter
+ALTER TABLE students
+drop primary key;
+
---
+++ Alter/db_and_tables.sql
@@ -0,0 +1,40 @@
+show databases;
+create database if not exists db_alter;
+use db_alter;
+SHOW Tables;
+DROP table students;
+
+
+-- Table 1
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    enrollment_date DATE
+);
+
+INSERT INTO students VALUES
+(1, 'Aman', 'Sharma', 20, 'Male', '2023-07-10'),
+(2, 'Riya', 'Verma', 21, 'Female', '2023-07-12'),
+(3, 'Kunal', 'Mehta', 19, 'Male', '2023-07-15');
+
+-- Table 2
+
+CREATE TABLE enrollments (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    enrollment_status VARCHAR(20),
+    constraint fk_student_id 
+    Foreign key(student_id) 
+    references students(student_id)
+);
+
+INSERT INTO enrollments VALUES
+(1, 1, 101, 'Active'),
+(2, 2, 102, 'Completed'),
+(3, 3, 103, 'Active');
+
