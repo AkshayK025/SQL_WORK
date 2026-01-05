559580ce635c69a989ede15eab0be8052d437892
AkshayK025 <askanchan314@gmail.com>
2026-01-05 10:23:43 +0530

    example of table and column rename added

---
+++ Alter/3_alter_rename.sql
@@ -0,0 +1,13 @@
+-- rename table students to student and again student to students use db_alter
+ALTER TABLE students
+rename to student;
+
+ALTER TABLE student
+rename to students;
+
+-- rename column first_name to name and again name to first_name from table student db_alter
+ALTER TABLE students
+change first_name f_name varchar(50);
+
+ALTER TABLE students
+change f_name first_name varchar(50);
